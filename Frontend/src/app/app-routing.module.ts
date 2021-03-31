1b76aaaed4284f24ba716716b1e8a64dffff9230
MaFe1401 <marcferre42@gmail.com>
2021-03-31 20:29:00 +0200

    inicio

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+{path:'',redirectTo:'home',pathMatch:'full'},
+{ path: 'register', loadChildren: () => import('./components/pages/register/register.module').then(m => m.RegisterModule) },
+{ path: 'login', loadChildren: () => import('./components/pages/login/login.module').then(m => m.LoginModule) }
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
